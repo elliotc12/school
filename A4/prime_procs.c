6f77d18054652bf5f2570051e8a659541f149af3
elliotc12 <elliotc12@gmail.com>
2015-08-03 00:06:15 -0700

    Add errthang. night.

--- A4/prime_procs.c
+++ A4/prime_procs.c
@@ -1,8 +1,49 @@
+#include <fcntl.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <unistd.h>
 
-#define MAX_PRIME 1000000
+#include "dynarr.h"
+
+#define MAX_PRIME 10000
 #define PROCESSES 16
+
+int* primes;
+int* happiness;
+
+int is_happy(int num) {
+	DynIntArr* arr = malloc(sizeof(DynIntArr));
+	arr_init(arr, 10);
+	
+	while(num != 1) {						// Loop until found happy or sad
+		int new_num = 0;
+		while (num / 10 != 0) {				// While >1 digit
+			new_num += pow(num % 10, 2);	// num % 10 = least sig. digit
+			num = num / 10;					// Chop off last digit
+		}
+		new_num += pow(num, 2);
+		num = new_num;
+		
+		int i;								// Check if already hit this #
+		for (i=0; i<arr->size; i++)
+		{
+			if (num == arr->data[i])
+			{
+				return 0; // is sad
+			}
+		}
+		append(arr, num);
+	}
+	return 1; // is happy
+	arr_free(arr);
+	free(arr);
+	
+	return 1;
+}
 
 int get_next_sieve_prime(int start) {
 	int m = start + 1;
@@ -14,34 +55,139 @@ int get_next_sieve_prime(int start) {
 	return -1;
 }
 
-void mark_multiples(int m) {
-	printf("new process where m=%d\n", m);
-	int j = 2;
+static void* find_happiness(int section) {
+	int MSIZE = MAX_PRIME * sizeof(int);
 	
-	while (m*j <= MAX_PRIME)
+	int i;
+	for (i = section*MAX_PRIME/PROCESSES + 1; i<=(section*MAX_PRIME/PROCESSES + MAX_PRIME/PROCESSES); i++) {
+		if (primes[i] && is_happy(i+1)) { happiness[i] = 1; }
+	}
+	
+	if (msync(happiness, MSIZE, MS_SYNC) == -1)
 	{
-		j++;
+		perror("mysnc: ");
+		exit(EXIT_FAILURE);
 	}
 	
 	exit(EXIT_SUCCESS);
 }
 
+void mark_multiples(int m) {
+	int MSIZE = MAX_PRIME * sizeof(int);
+	int j = 2;
+	
+	/*sem_t* num_term_children;
+	
+	num_term_children = sem_open("num_term_children", 0);
+	if (num_term_children == SEM_FAILED)
+	{
+		perror("sem_open: ");
+		exit(EXIT_FAILURE);
+	}*/
+	
+	while (m*j <= MAX_PRIME)
+	{
+		primes[m*j-1] = 0;
+		j++;
+	}
+	
+	if (msync(primes, MSIZE, MS_SYNC) == -1)
+	{
+		perror("mysnc: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	//sem_post(num_term_children);
+	exit(EXIT_SUCCESS);
+}
 
 int main() {
 	int m = 1;									// Current sieve prime, initialized to 1
+	int MSIZE = MAX_PRIME * sizeof(int);
+	int child_pids[PROCESSES];
 	
-	int primes[MAX_PRIME];
-	int happiness[MAX_PRIME];
+	sem_t* num_term_children;
 	
-	int i;										// Mark all numbers as prime and unhappy
-	for (i=0; i<MAX_PRIME; i++) 
+	num_term_children = sem_open("num_term_children", O_CREAT, S_IRUSR | S_IWUSR, 0);
+	if (num_term_children == SEM_FAILED)
 	{
-		primes[i] = 1;
-		happiness[i] = 0;
+		perror("sem_open: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	int fd = open("prime_procs_temp_shm", O_RDWR | O_CREAT | O_TRUNC, 0755);
+	if (fd == -1)
+	{
+		perror("open: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	int fd_h = open("prime_procs_temp_shm_happiness", O_RDWR | O_CREAT | O_TRUNC, 0755);
+	if (fd == -1)
+	{
+		perror("open: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (ftruncate(fd, MSIZE) == -1)
+	{
+		perror("ftruncate: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (ftruncate(fd_h, MSIZE) == -1)
+	{
+		perror("ftruncate: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	primes = mmap(NULL, MSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (primes == MAP_FAILED)
+	{
+		perror("mmap: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	happiness = mmap(NULL, MSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_h, 0);
+	if (happiness == MAP_FAILED)
+	{
+		perror("mmap: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (close(fd) == -1)
+	{
+		perror("close: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (close(fd_h) == -1)
+	{
+		perror("close: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	int a;
+	for (a=0; a<MAX_PRIME; a++)
+	{
+		primes[a] = 1;
+		happiness[a] = 0;
 	}
 	primes[0] = 0;
 	
-	int c;										// Initialize threads and thread structures
+	if (msync(primes, MSIZE, MS_SYNC) == -1)
+	{
+		perror("mysnc: ");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (msync(happiness, MSIZE, MS_SYNC) == -1)
+	{
+		perror("mysnc: ");
+		exit(EXIT_FAILURE);
+	}
+		
+	int c;
 	for (c=0; c<PROCESSES; c++)
 	{
 		m = get_next_sieve_prime(m);
@@ -58,71 +204,83 @@ int main() {
 		} 
 		else
 		{
+			child_pids[c] = p;
+			continue;
+		}
+	}
+
+	while ((m = get_next_sieve_prime(m)) != -1)
+	{
+		//sem_wait(num_term_children);							// Block until a process terminates
+		
+		wait(NULL);
+		
+		int b = 0;
+		while (waitpid(child_pids[b], NULL, WNOHANG) == 0) { b++; }
+		if (b >= PROCESSES) {
+			fprintf(stderr, "Error. num_term_children incremented but no children exited.\n");
+			exit(EXIT_FAILURE);
+		}
+		
+		int p = fork();
+		if (p == 0)
+		{
+			mark_multiples(m);
+		} 
+		else if (p == -1)
+		{
+			perror("fork: ");
+			exit(EXIT_FAILURE);
+		} 
+		else
+		{
+			child_pids[b] = p;
 			continue;
 		}
 		
-		//fork process, send child to mark_multiples with m as parameter. Have it mark shared memory array.
 	}
 	
-	/*
-	while ((m = get_next_sieve_prime(m)) != -1)
+	int g;											// Wait for all processes to terminate
+	for (g=0; g<PROCESSES; g++)
 	{
-		//wait() for child to die? or use a SIGCHLD handler to wait for kids to die, decrement in handler
-		int a = 0;
-		while (t_info[a].state == THREAD_LIVE) { a++; }
-		if (a >= THREADS)
+		waitpid(child_pids[g], NULL, 0);
+	}
+
+
+	int f;
+	for (f=0; f<PROCESSES; f++)
+	{	
+		int p = fork();
+		if (p == 0)
 		{
-			fprintf(stderr, "Error. All threads alive but thread_termination signaled.\n");
+			find_happiness(f);
+		} 
+		else if (p == -1)
+		{
+			perror("fork: ");
 			exit(EXIT_FAILURE);
-		}
-		
-		pthread_join(t_info[a].tid, NULL);
-		
-		t_info[a].state = THREAD_LIVE;
-		t_info[a].sieve_prime = m;
-		
-		if (pthread_create(&t_info[a].tid, NULL, mark_multiples, &t_info[a]) != 0) {
-			perror("pthread_create: ");
-			exit(EXIT_FAILURE);
+		} 
+		else
+		{
+			child_pids[f] = p;
+			continue;
 		}
 	}
+
+	int e;											// Wait for all processes to terminate again
+	for (e=0; e<PROCESSES; e++)
+	{
+		waitpid(child_pids[e], NULL, 0);
+	}
 	
-	
-	/*
 	int d;
-	for (d=0; d<THREADS; d++)						// Join all threads to main(), then relaunch them with new task
+	for (d=0; d<MAX_PRIME; d++)
 	{
-		while (t_info[d].state != THREAD_DEAD) { pthread_cond_wait(&thread_termination, &mutex); }
-		pthread_join(t_info[d].tid, NULL);
-		
-		t_info[d].num = d;
-		t_info[d].state = THREAD_LIVE;
-		
-		if (pthread_create(&t_info[d].tid, NULL, find_happiness, &t_info[d]) != 0)
-		{
-			perror("pthread_create: ");
-			exit(EXIT_FAILURE);
-		}
+		if (happiness[d] == 1) { printf("%d\n", d+1); }
 	}
+
 	
-	int numAlive = THREADS;
-	while (numAlive > 0) 
-	{
-		pthread_cond_wait(&thread_termination, &mutex);			// threads may terminate when main() blocks here
-		
-		int e;
-		for (e=0; e<THREADS; e++)
-		{
-			if (t_info[e].state == THREAD_DEAD)
-			{
-				pthread_join(t_info[e].tid, NULL);
-				t_info[e].state = THREAD_JOINED;
-				numAlive--;
-			}
-		}
-	}
-	
-	int fd;
+	/*int fd;
 	if ((fd = open("happy_primes", O_WRONLY | O_CREAT | O_TRUNC, 0755)) == -1)
 	{
 		perror("error open: ");
@@ -149,4 +307,6 @@ int main() {
 	}
 	
 	*/
+	
+	sem_unlink("num_term_children");
 }
