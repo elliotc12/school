a467b6683107eda568c713798fe1526ca8b23498
Elliott Capek <capeke@os-class.engr.oregonstate.edu>
2015-08-14 03:35:04 -0700

    finished.

--- A5/manage.py
+++ A5/manage.py
@@ -9,6 +9,7 @@ import subprocess
 import threading
 import time
 import os
+import signal
 
 SERVER_SOCKNAME = "/tmp/CS344_manage_sock"
 MAX_READ_SIZE = 8192
@@ -78,10 +79,13 @@ def handle_compute_connection(conn, json_dict):
     json_data = conn.recv( (end - start) + 13)
     parsed_data = json_decode(json_data)
 
-    for i in range(0, len(parsed_data["data"])):
-        if (parsed_data["data"][i] == '1'):
-           perf_nums.append(i+start)
+    try:
+        for i in range(0, len(parsed_data["data"])):
+            if (parsed_data["data"][i] == '1'):
+                perf_nums.append(i+start)
 
+    except:
+        pass
     processes.remove(proc_dict)
 
 def handle_term_connection(conn, json_dict):
@@ -100,6 +104,16 @@ def handle_exit():
     subprocess.call(["rm", SERVER_SOCKNAME])
     print("manage.py exiting.")
     os._exit(0)
+
+def sig_handle_exit(one, two):
+    global e
+    e.set()
+    e.clear()
+    time.sleep(1)
+    subprocess.call(["rm", SERVER_SOCKNAME])
+    print("manage.py exiting.")
+    os._exit(0)
+
 
 def handle_new_connection(conn):
     msg = conn.recv(MAX_READ_SIZE)
@@ -129,6 +143,8 @@ if __name__ == "__main__":
     # thread to listen for new connections, then update internal data structure that maintains compute processes and their performance
     # once a new communication has started, create a thread that sends that process commands, waits for it to finish, then sends more commands
 
+    signal.signal(signal.SIGINT, sig_handle_exit)
+    signal.signal(signal.SIGQUIT, sig_handle_exit)
     subprocess.call(["rm", "-f", SERVER_SOCKNAME])
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.bind(SERVER_SOCKNAME)
