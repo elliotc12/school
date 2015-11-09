024c671d015aa2ee156f14782a781ff3d9594e06
elliotc12 <elliotc12@gmail.com>
2015-11-08 16:56:04 -0800

    add plot.py

--- comp_model/plot.py
+++ comp_model/plot.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
 data = np.genfromtxt("data.txt", delimiter=" ")
 plt.ion()
@@ -21,7 +22,11 @@ i = 0
 x = range(0, int(math.sqrt(len(data))))
 y = range(0, int(math.sqrt(len(data))))
 X, Y = np.meshgrid(x, y)
-Z = np.reshape(data, (math.sqrt(len(data)), math.sqrt(len(data))))
+#Z = np.reshape(data, (math.sqrt(len(data)), math.sqrt(len(data))))
+
+Z1 = mlab.bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
+Z2 = mlab.bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
+Z = 10.0 * (Z2 - Z1)
 
 print X, Z
 
